30ce33606153c89a016ed52f72f5b50e5ddb09eb
kirilldyachkovskiy <kirilldyachkovskiy@gmail.com>
2023-09-10 17:38:47 +0300

    feat(BinaryTree): add deserialization algorithm

--- utils/binaryTree/serializatoin.go
+++ utils/binaryTree/serializatoin.go
@@ -29,6 +29,57 @@ func (bt *BinaryTree[T]) Serialize() []T {
 	return result
 }
 
+func Deserialize[T comparable](data []T) BinaryTree[T] {
+	genZero := getZero[T]()
+
+	var stack []*BinaryTree[T]
+
+	isLastNil := false
+
+	for _, item := range data {
+		if item != genZero {
+			newNode := &BinaryTree[T]{Value: item}
+
+			if len(stack) == 0 {
+				stack = append(stack, newNode)
+				continue
+			}
+
+			lastNode := stack[len(stack)-1]
+
+			if isLastNil {
+				lastNode.Right = newNode
+				isLastNil = false
+			} else {
+				lastNode.Left = newNode
+			}
+
+			stack = append(stack, newNode)
+			continue
+		}
+
+		if len(stack) == 0 {
+			continue
+		}
+
+		if isLastNil {
+			if len(stack) == 1 {
+				return *stack[0]
+			}
+
+			stack = stack[:len(stack)-1]
+
+			for len(stack) > 1 && stack[len(stack)-1].Right != nil {
+				stack = stack[:len(stack)-1]
+			}
+		} else {
+			isLastNil = true
+		}
+	}
+
+	return *stack[0]
+}
+
 func getZero[T any]() T {
 	var result T
 	return result
--- utils/binaryTree/serializatoin_test.go
+++ utils/binaryTree/serializatoin_test.go
@@ -108,3 +108,122 @@ func TestBinaryTree_Serialize(t *testing.T) {
 		})
 	}
 }
+
+func TestDeserialize(t *testing.T) {
+	type args[T comparable] struct {
+		data []T
+	}
+	type testCase[T comparable] struct {
+		name string
+		args args[T]
+		want BinaryTree[T]
+	}
+	tests := []testCase[byte]{
+		{
+			name: "base case",
+			args: args[byte]{
+				data: []byte{1, 0, 0},
+			},
+			want: BinaryTree[byte]{
+				Value: 1,
+			},
+		},
+		{
+			name: "case with left node",
+			args: args[byte]{
+				data: []byte{1, 2, 0, 0, 0},
+			},
+			want: BinaryTree[byte]{
+				Value: 1,
+				Left: &BinaryTree[byte]{
+					Value: 2,
+				},
+			},
+		},
+		{
+			name: "case with right node",
+			args: args[byte]{
+				data: []byte{1, 0, 3, 0, 0},
+			},
+			want: BinaryTree[byte]{
+				Value: 1,
+				Right: &BinaryTree[byte]{
+					Value: 3,
+				},
+			},
+		},
+		{
+			name: "case with both nodes",
+			args: args[byte]{
+				data: []byte{1, 2, 0, 0, 3, 0, 0},
+			},
+			want: BinaryTree[byte]{
+				Value: 1,
+				Left: &BinaryTree[byte]{
+					Value: 2,
+				},
+				Right: &BinaryTree[byte]{
+					Value: 3,
+				},
+			},
+		},
+		{
+			name: "case with many nodes",
+			args: args[byte]{
+				data: []byte{1, 2, 4, 0, 0, 5, 0, 11, 0, 0, 3, 6, 0, 13, 0, 0, 0},
+			},
+			want: BinaryTree[byte]{
+				Value: 1,
+				Left: &BinaryTree[byte]{
+					Value: 2,
+					Left: &BinaryTree[byte]{
+						Value: 4,
+					},
+					Right: &BinaryTree[byte]{
+						Value: 5,
+						Right: &BinaryTree[byte]{
+							Value: 11,
+						},
+					},
+				},
+				Right: &BinaryTree[byte]{
+					Value: 3,
+					Left: &BinaryTree[byte]{
+						Value: 6,
+						Right: &BinaryTree[byte]{
+							Value: 13,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "case with one long hand",
+			args: args[byte]{
+				data: []byte{1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0},
+			},
+			want: BinaryTree[byte]{
+				Value: 1,
+				Left: &BinaryTree[byte]{
+					Value: 2,
+					Left: &BinaryTree[byte]{
+						Value: 3,
+						Left: &BinaryTree[byte]{
+							Value: 4,
+							Left: &BinaryTree[byte]{
+								Value: 5,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Deserialize(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Deserialize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
